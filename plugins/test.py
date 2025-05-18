2bef03f15a357d67341c24c021af6b9792d3bc4a
NeonAnurag <justasneon@gmail.com>
2025-05-18 14:21:25 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -45,11 +45,11 @@ class CLIENT:
      elif not msg.forward_date:
        return await msg.reply_text("<b>__This is not a forward message__</b>")
      elif str(msg.forward_from.id) != "93372553":
-       return await msg.reply_text("<b>This message was not forward from bot father</b>")
+       return await msg.reply_text("<b>__This message was not forward from BotFather__</b>")
      bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
      bot_token = bot_token[0] if bot_token else None
      if not bot_token:
-       return await msg.reply_text("<b>There is no bot token in that message</b>")
+       return await msg.reply_text("<b>__There is no Bot token in message__</b>")
      try:
        _client = Client("BOT", Config.API_ID, Config.API_HASH, bot_token=bot_token, in_memory=True)
        client = await _client.start()
@@ -70,46 +70,46 @@ class CLIENT:
 
   async def add_session(self, bot, message):
      user_id = int(message.from_user.id)
-     text = "<b>⚠️ DISCLAIMER ⚠️</b>\n\n<code>you can use your session for forward message from private chat to another chat.\nPlease add your pyrogram session with your own risk. Their is a chance to ban your account. My developer is not responsible if your account may get banned.</code>"
+     text = "<b>⚠️ DISCLAIMER ⚠️</b>\n\n<code>__you can use your session for forward message from private chat to another chat.\nPlease add your pyrogram session with your own risk. Their is a chance to ban your account. My developer is not responsible if your account may get banned.__</code>"
      await bot.send_message(user_id, text=text)
-     phone_number_msg = await bot.ask(chat_id=user_id, text="<b>Please send your phone number which includes country code</b>\n<b>Example:</b> <code>+13124562345</code>")
+     phone_number_msg = await bot.ask(chat_id=user_id, text="<b>__Please send your phone number which includes country code__</b>\n<b>Example:</b> <code>+13124562345</code>")
      if phone_number_msg.text=='/cancel':
-        return await phone_number_msg.reply('<b>process cancelled !</b>')
+        return await phone_number_msg.reply('<b>__Process cancelled !__</b>')
      phone_number = phone_number_msg.text
      client = Client(":memory:", Config.API_ID, Config.API_HASH)
      await client.connect()
-     await phone_number_msg.reply("Sending OTP...")
+     await phone_number_msg.reply("__Sending OTP...__ 👀")
      try:
         code = await client.send_code(phone_number)
-        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \n\nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n**Enter /cancel to cancel The Procces**", filters=filters.text, timeout=600)
+        phone_code_msg = await bot.ask(user_id, "**__Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format__**. \n\n**__If OTP is__** `12345`, **__please send it as__** `1 2 3 4 5`.\n\n**__Enter__ /cancel __to cancel The Procces__**", filters=filters.text, timeout=600)
      except PhoneNumberInvalid:
         await phone_number_msg.reply('`PHONE_NUMBER` **is invalid.**')
         return
      if phone_code_msg.text=='/cancel':
-        return await phone_code_msg.reply('<b>process cancelled !</b>')
+        return await phone_code_msg.reply('<b>__Process cancelled !__</b>')
      try:
         phone_code = phone_code_msg.text.replace(" ", "")
         await client.sign_in(phone_number, code.phone_code_hash, phone_code)
      except PhoneCodeInvalid:
-        await phone_code_msg.reply('**OTP is invalid.**')
+        await phone_code_msg.reply('**OTP __is invalid.__**')
         return
      except PhoneCodeExpired:
-        await phone_code_msg.reply('**OTP is expired.**')
+        await phone_code_msg.reply('**OTP __is expired__.**')
         return
      except SessionPasswordNeeded:
-        two_step_msg = await bot.ask(user_id, '**Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel The Procces**', filters=filters.text, timeout=300)
+        two_step_msg = await bot.ask(user_id, '**__Your account has enabled two-step verification. Please provide the password.\n\nEnter__ /cancel __to cancel The Procces__**', filters=filters.text, timeout=300)
         if two_step_msg.text=='/cancel':
-            return await two_step_msg.reply('<b>process cancelled !</b>')
+            return await two_step_msg.reply('<b>__Process cancelled !__</b>')
         try:
            password = two_step_msg.text
            await client.check_password(password=password)
         except PasswordHashInvalid:
-           await two_step_msg.reply('**Invalid Password Provided**')
+           await two_step_msg.reply('**__Invalid Password Provided__**')
            return
      string_session = await client.export_session_string()
      await client.disconnect()
      if len(string_session) < SESSION_STRING_SIZE:
-        return await msg.reply('<b>invalid session sring</b>')
+        return await msg.reply('<b>__invalid session sring__</b>')
      try:
        _client = Client("USERBOT", self.api_id, self.api_hash, session_string=string_session)
        client = await _client.start()
@@ -127,24 +127,16 @@ class CLIENT:
      await db.add_userbot(details)
      return True
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 @Client.on_message(filters.private & filters.command('reset'))
 async def forward_tag(bot, m):
    default = await db.get_configs("01")
    await db.update_configs(m.from_user.id, default)
-   await m.reply("successfully settings reseted ✔️")
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+   await m.reply("__Successfully settings Reseted ✔️__")
 
 @Client.on_message(filters.command('resetall') & filters.user(Config.BOT_OWNER))
 async def resetall(bot, message):
   users = await db.get_all_users()
-  sts = await message.reply("**processing**")
+  sts = await message.reply("**__Processing__**")
   TEXT = "total: {}\nsuccess: {}\nfailed: {}\nexcept: {}"
   total = success = failed = already = 0
   ERRORS = []
@@ -165,17 +157,10 @@ async def resetall(bot, message):
      await message.reply(ERRORS[:100])
   await sts.edit("completed\n" + TEXT.format(total, success, failed, already))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def get_configs(user_id):
   configs = await db.get_configs(user_id)
   return configs
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def update_configs(user_id, key, value):
   current = await db.get_configs(user_id)
@@ -184,10 +169,6 @@ async def update_configs(user_id, key, value):
   else: 
      current['filters'][key] = value
   await db.update_configs(user_id, current)
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def iter_messages(
     self,
@@ -213,19 +194,11 @@ async def iter_messages(
                     
                 current += 1
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 async def get_client(bot_token, is_bot=True):
   if is_bot:
     return Client("BOT", Config.API_ID, Config.API_HASH, bot_token=bot_token, in_memory=True)
   else:
     return Client("USERBOT", Config.API_ID, Config.API_HASH, session_string=bot_token)
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 def parse_buttons(text, markup=True):
     buttons = []
@@ -249,6 +222,4 @@ def parse_buttons(text, markup=True):
        buttons = InlineKeyboardMarkup(buttons)
     return buttons if buttons else None
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @MyselfNeon
