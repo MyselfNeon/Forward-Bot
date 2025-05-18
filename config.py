51a333c4d0489c91e70ec062e7722fc5d5e8beb8
NeonAnurag <justasneon@gmail.com>
2025-05-18 09:20:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Don't Remove Credit
 from os import environ 
 
 class Config:
@@ -13,10 +10,6 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "MyselfNeon")
     BOT_OWNER = int(environ.get("BOT_OWNER", "841851780"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 class temp(object): 
     lock = {}
     CANCEL = {}
@@ -24,6 +17,4 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @MyselfNeon
